4c68d74d0090afa07de23a338de7da81cff98a1d
matheus-baraujo <37562463+matheus-baraujo@users.noreply.github.com>
2018-08-07 18:32:52 -0300

    Create exercicio5.java

---
+++ exercicio5.java
@@ -0,0 +1,16 @@
+class exercicio5{
+    static void main(String args[]){
+        Random random = new Random();
+        int x= ran.nextInt(11);
+        //gerando um numero aleatorio ate no max 10
+        
+        while(x!=1){
+           if(x%2==0){
+              x=x/2;
+           }
+           else{
+              x=(x*3)+1;
+           }
+        }
+    }
+}
